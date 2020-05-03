9e8daa0ee109c90e0fb30cd1dc96a6f5f3d1ec21
tkeer <taukeer.liaqat@gmail.com>
2020-05-03 16:22:15 +0500

    routes groups in "bindings" middleware

--- src/routes.php
+++ src/routes.php
@@ -3,8 +3,9 @@
 declare(strict_types=1);
 
 use Tkeer\Mailbase\MailController;
+use Illuminate\Routing\Middleware\SubstituteBindings;
 
-Route::group(['as' => 'mailbase::', 'prefix' => 'mailbase'], function () {
+Route::group(['as' => 'mailbase::', 'prefix' => 'mailbase', 'middleware' => SubstituteBindings::class], function () {
     Route::get('/', MailController::class . '@index')->name('index');
     Route::get('/{mailbase}', MailController::class . '@show')->name('show');
 });
